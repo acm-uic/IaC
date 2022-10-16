1f393478c6c057fbf7701c16c55d56a51625e7f5
Chase 李 <chase@emiya.io>
2022-10-16 01:09:12 -0500

    Correcting Terraform formatting

--- azure/terraform/stacks/acm-general/backend.tf
+++ azure/terraform/stacks/acm-general/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "acm-hybridcloud"
-      storage_account_name = "acmhybridstore"
-      container_name       = "acm-terraform-state"
-      key                  = "acm-general.tfstate"
+    storage_account_name = "acmhybridstore"
+    container_name       = "acm-terraform-state"
+    key                  = "acm-general.tfstate"
   }
 }
