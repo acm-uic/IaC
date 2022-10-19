6531b776d183f43bd5c5dfb93849ceb75a8ad5d7
Chase 李 <clee231@uic.edu>
2022-10-18 23:43:34 -0500

    Revert "Adding a test demo record (#16)" (#17)
    This reverts commit 6e385a3de0810ecd31ceb594535f810138c1b5b5.

--- azure/terraform/stacks/acm-general/dns.tf
+++ azure/terraform/stacks/acm-general/dns.tf
@@ -3,10 +3,3 @@ data "azurerm_dns_zone" "acmuic_app" {
   resource_group_name = azurerm_resource_group.acm_general.name
 }
 
-resource "azurerm_dns_cname_record" "chase_demo" {
-  name                = "chasedemo"
-  zone_name           = data.azurerm_dns_zone.acmuic_app.name
-  resource_group_name = azurerm_resource_group.acm_general.name
-  ttl                 = 300
-  record              = "sysadmin.acmapp.tech"
-}
