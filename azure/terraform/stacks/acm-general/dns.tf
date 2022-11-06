2ea71f046f16a91bf343fb453254396bb30cf540
Chase 李 <chase@acm.cs.uic.edu>
2022-11-06 10:43:55 -0800

    🌐 Adding DNS entry for flourishconf.acmuic.org (#19)

--- azure/terraform/stacks/acm-general/dns.tf
+++ azure/terraform/stacks/acm-general/dns.tf
@@ -3,3 +3,16 @@ data "azurerm_dns_zone" "acmuic_app" {
   resource_group_name = azurerm_resource_group.acm_general.name
 }
 
+data "azurerm_dns_zone" "acmuic_org" {
+  name                = "acmuic.org"
+  resource_group_name = azurerm_resource_group.acm_general.name
+}
+
+resource "azurerm_dns_cname_record" "flourishconf_acmuic_org" {
+  name                = "flourishconf"
+  zone_name           = data.azurerm_dns_zone.acmuic_org.name
+  resource_group_name = azurerm_resource_group.acm_general.name
+  ttl                 = 300
+  record              = "docker1.acmuic.org"
+}
+
