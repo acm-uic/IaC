d7e64b5c843ed87cc7089700155e40267905f8e0
Chase 李 <chase@acm.cs.uic.edu>
2023-03-12 23:12:36 -0500

    Creating new service account for SIGSysAdmin Demo (#22)
    * Creating new service account for SIGSysAdmin Demo.
    
    * Fixing linting.
    
    * Adding more strict Github Actions
    
    * Fixing pathing error
    
    * Setting Terraform version
    
    * Consistently referencing environment variable
    
    ---------
    
    Co-authored-by: Chase 李 <chase@emiya.io>

---
+++ azure/terraform/stacks/acm-general/service_principal.tf
@@ -0,0 +1,18 @@
+data "azuread_client_config" "current" {}
+
+resource "azuread_application" "terraform_sysadmin_demo" {
+  display_name = "terraform-sysadmindemo-svc"
+  owners       = [data.azuread_client_config.current.object_id]
+}
+
+resource "azuread_service_principal" "terraform_sysadmin_demo" {
+  application_id               = azuread_application.terraform_sysadmin_demo.application_id
+  app_role_assignment_required = false
+  owners                       = [data.azuread_client_config.current.object_id]
+}
+
+resource "azuread_service_principal_password" "terraform_sysadmin_demo" {
+  display_name         = "rbac-sysadmindemo"
+  service_principal_id = azuread_service_principal.terraform_sysadmin_demo.object_id
+  end_date_relative    = "240h"
+}
