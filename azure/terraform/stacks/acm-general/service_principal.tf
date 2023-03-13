ad5e9e869b69b440d864f48e6eb12dc4716aa3fe
Chase 李 <chase@acm.cs.uic.edu>
2023-03-13 00:36:27 -0500

    Adding additional permissions for the demo service account

--- azure/terraform/stacks/acm-general/service_principal.tf
+++ azure/terraform/stacks/acm-general/service_principal.tf
@@ -1,5 +1,7 @@
 data "azuread_client_config" "current" {}
 
+data "azurerm_subscription" "primary" {}
+
 resource "azuread_application" "terraform_sysadmin_demo" {
   display_name = "terraform-sysadmindemo-svc"
   owners       = concat([data.azuread_client_config.current.object_id], var.additional_owner_ids)
@@ -15,5 +17,17 @@ resource "azuread_application_password" "terraform_sysadmin_demo" {
   application_object_id = azuread_application.terraform_sysadmin_demo.object_id
   display_name          = "rbac-sysadmindemo-apppass"
   end_date_relative     = "240h"
+}
+
+resource "azurerm_role_assignment" "terraform_sysadmin_demo_reader" {
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = "Reader"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
+resource "azurerm_role_assignment" "terraform_sysadmin_demo_dns" {
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = "DNS Zone Contributor"
+  principal_id         = data.azurerm_client_config.current.object_id
 }
 
