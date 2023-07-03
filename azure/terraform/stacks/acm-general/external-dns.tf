791464a6904c217fc0bb0c005b01b33b9165287e
Chase 李 <chase@acm.cs.uic.edu>
2023-07-03 02:17:06 -0500

    Formatting. Removing relocated file.

--- azure/terraform/stacks/acm-general/external-dns.tf
+++ azure/terraform/stacks/acm-general/external-dns.tf
@@ -15,27 +15,27 @@ resource "azuread_service_principal" "externaldns" {
 }
 
 resource "azuread_service_principal_password" "externaldns" {
-  display_name = "${var.external_dns_namespace}Password"
+  display_name         = "${var.external_dns_namespace}Password"
   service_principal_id = azuread_service_principal.externaldns.object_id
 }
 
 resource "azurerm_role_assignment" "externaldns_reader" {
-  scope = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/acm-general/providers/Microsoft.Network/dnszones/${var.public_domain_suffixes[0]}"
+  scope                = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/acm-general/providers/Microsoft.Network/dnszones/${var.public_domain_suffixes[0]}"
   role_definition_name = "Reader"
-  principal_id = azuread_service_principal.externaldns.object_id
+  principal_id         = azuread_service_principal.externaldns.object_id
 }
 
 resource "azurerm_role_assignment" "externaldns_contributor" {
-  scope = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/acm-general/providers/Microsoft.Network/dnszones/${var.public_domain_suffixes[0]}"
+  scope                = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/acm-general/providers/Microsoft.Network/dnszones/${var.public_domain_suffixes[0]}"
   role_definition_name = "Contributor"
-  principal_id = azuread_service_principal.externaldns.object_id
+  principal_id         = azuread_service_principal.externaldns.object_id
 }
 
 output "externaldns_sp_keyid" {
-    value = azuread_service_principal_password.externaldns.key_id
-  }
+  value = azuread_service_principal_password.externaldns.key_id
+}
 
 output "externaldns_sp_password" {
-    value = azuread_service_principal_password.externaldns.value
-    sensitive = true
-  }
+  value     = azuread_service_principal_password.externaldns.value
+  sensitive = true
+}
--- azure/terraform/stacks/acm-general/variables.tf
+++ azure/terraform/stacks/acm-general/variables.tf
@@ -5,19 +5,19 @@ variable "additional_owner_ids" {
 }
 
 variable "public_domain_suffixes" {
-  type = list(string)
+  type        = list(string)
   description = "The list of public domains used for DNS, that are managed by the Kubernetes cluster"
 }
 
 variable "external_dns_provider" {
-  type = string
+  type        = string
   description = "Specifies the platform to interact with"
-  default = "azure"
+  default     = "azure"
 }
 
 variable "external_dns_namespace" {
-  type = string
+  type        = string
   description = "Specifies the namespace to create for externaldns"
-  default = "externaldns"
+  default     = "externaldns"
 }
 
