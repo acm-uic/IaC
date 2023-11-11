6e81908c4cca3b1fbbfe17bb8cdb9945ecc9a0b4
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2023-11-11 00:19:29 -0600

    Typesense on k8s stack
    Co-authored-by: Chase 李 <chase@acm.cs.uic.edu>

--- azure/terraform/stacks/typesense/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "acm-hybridcloud"
-    storage_account_name = "acmhybridstore"
-    container_name       = "acm-terraform-state"
-    key                  = "typesense-stack.tfstate"
-    snapshot             = true
-  }
-}
