7a9bbb0b1bc2373a8b2ff907b496237fb5337525
Soham S Gumaste <sgumas2@uic.edu>
2024-01-16 14:17:51 -0600

    Add SparkHacks 2024

--- shell.nix
+++ shell.nix
@@ -3,7 +3,9 @@ with import <nixpkgs>{};
 let PROJECT_ROOT=builtins.toString ./.; in
 mkShell {
 	name="ACM IaC";
-	packages=[terraform ansible];
+	packages=[
+   # terraform # Will compile from source.
+    ansible];
 
 
 	KUBECONFIG="${PROJECT_ROOT}/k8s-kubeconfig";
