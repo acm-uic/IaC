76ad3e02a6e94f1df4bbed48a29af3a1a0c45a1d
lamtnguyen989 <lamtnguyen989@gmail.com>
2024-11-13 18:51:47 -0600

    Uwupdating thewe tf files

--- azure/terraform/stacks/sysadmin-demo/MaxNguyendns.tf
+++ azure/terraform/stacks/sysadmin-demo/MaxNguyendns.tf
@@ -1,7 +1,7 @@
 resource "azurerm_dns_aaaa_record" "Overlord_MaxNguyen" {
-  name                = "Overlord_MaxNguyen"
+  name                = "overlord-max"
   zone_name           = data.azurerm_dns_zone.acmuic_org.name
   resource_group_name = data.azurerm_resource_group.acm_general.name
-  ttl                 = 30
+  ttl                 = 300
   records             = ["fdc5:2d46:cdc1:b358::1"]
 }
