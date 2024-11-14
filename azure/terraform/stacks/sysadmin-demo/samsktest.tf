34d74e331733a8ca33e17cad4b6469fb3c851dab
Samuel Skean <sskean2@uic.edu>
2024-11-14 00:37:54 +0000

    Test DNS record.

---
+++ azure/terraform/stacks/sysadmin-demo/samsktest.tf
@@ -0,0 +1,7 @@
+resource "azurerm_dns_aaaa_record" "samsktest" {
+  name                = "samsk"
+  zone_name           = data.azurerm_dns_zone.acmuic_org.name
+  resource_group_name = data.azurerm_dns_zone.acmuic_org.resource_group_name
+  ttl                 = 300
+  records             = ["fdc5:2d46:cdc1:b358:5a47:caff:fe77:e556/64"]
+}
