dc28d7310f770a2532bbe4c2bfcd1674d5ce11a8
lamtnguyen989 <145540332+lamtnguyen989@users.noreply.github.com>
2024-11-13 19:36:54 -0600

    Change with web editor

--- azure/terraform/stacks/sysadmin-demo/maxnguyendns.tf
+++ azure/terraform/stacks/sysadmin-demo/maxnguyendns.tf
@@ -3,5 +3,5 @@ resource "azurerm_dns_aaaa_record" "overlord-max-nguyen" {
   zone_name           = data.azurerm_dns_zone.acmuic_org.name
   resource_group_name = data.azurerm_resource_group.acm_general.name
   ttl                 = 30
-  records             = ["fdc5:2d46:cdc1:b358::1"] 
+  records             = ["fdc5:2d46:cdc1:b358::1"]
 }
