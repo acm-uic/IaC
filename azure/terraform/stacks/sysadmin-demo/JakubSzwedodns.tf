cf980dd3c111c429438d8f5fcc33d5607b2f9b64
amc-js <jszwe@uic.edu>
2024-11-13 19:16:00 -0600

    Sowwwy Chase

--- azure/terraform/stacks/sysadmin-demo/JakubSzwedodns.tf
+++ azure/terraform/stacks/sysadmin-demo/JakubSzwedodns.tf
@@ -1,7 +1,7 @@
 resource "azurerm_dns_aaaa_record" "notExamnple" {
   name                = "DrPepper"
-  zone_name           = azurerm_dns_zone.example.name
-  resource_group_name = azurerm_resource_group.example.name
+  zone_name           = data.azurerm_dns_zone.acmuic_org.name
+  resource_group_name = data.azurerm_resource_group.acm_general.name
   ttl                 = 300
   records             = ["fdc5:2d46:cdc1:b358:5a47:caff:fe77:e556"]
 }
