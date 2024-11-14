458a3a914c6b9eddbccc4157ef45102b393ae558
Civita2107 <f.civitareale@gmail.com>
2024-11-13 18:44:10 -0600

    Modified quada.tf

--- azure/terraform/stacks/sysadmin-demo/quada.tf
+++ azure/terraform/stacks/sysadmin-demo/quada.tf
@@ -1,7 +1,7 @@
 resource "azurerm_dns_aaaa_record" "example" {
   name                = "test"
-  zone_name           = azurerm_dns_zone.example.name
-  resource_group_name = azurerm_resource_group.example.name
+  zone_name           = acmuic.org
+  resource_group_name = data.azurerm_resource_group.acm_general.name
   ttl                 = 300
   records             = ["2001:db8::1:0:0:1"]
 }
