e0af0e902d752ccf5c32d9ade8ce478fc4449076
Samuel Skean <sskean2@uic.edu>
2024-11-14 00:40:15 +0000

    Amendment of the definition, specifying which computer it's connected to.

--- azure/terraform/stacks/sysadmin-demo/samsktest.tf
+++ azure/terraform/stacks/sysadmin-demo/samsktest.tf
@@ -3,5 +3,5 @@ resource "azurerm_dns_aaaa_record" "samsktest" {
   zone_name           = data.azurerm_dns_zone.acmuic_org.name
   resource_group_name = data.azurerm_dns_zone.acmuic_org.resource_group_name
   ttl                 = 300
-  records             = ["fdc5:2d46:cdc1:b358:5a47:caff:fe77:e556/64"]
+  records             = ["fdc5:2d46:cdc1:b358:5a47:caff:fe77:e556/64"] # Coffee
 }
