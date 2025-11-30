5f47d12de610ef63611e4c5464dae69d30d518f8
Chase 李 <chase@acm.cs.uic.edu>
2025-11-30 10:04:38 -0600

    Bug/Fix pfSense ACME renewals (#125)
    This adds Terraform resource definitions to manage the
    `pfSenseServiceApp`.
    
    This application is in charge of providing pfSense credentials in Azure
    to manage DNS records in the `acmuic.org` DNS zone.

---
+++ azure/terraform/stacks/acm-general/pfsense.tf
@@ -0,0 +1,40 @@
+# https://github.com/acmesh-official/acme.sh/wiki/How-to-use-Azure-DNS
+#
+# This is a service principal that is used by PFSense to manage DNS records
+# for the ACME challenges.
+
+resource "azuread_application" "pfsense" {
+  display_name = "pfsenseServiceApp"
+  owners       = var.additional_owner_ids
+}
+
+resource "azuread_service_principal" "pfsense" {
+  client_id   = azuread_application.pfsense.client_id
+  description = "Service Principal for on-prem pfSense"
+  owners      = var.additional_owner_ids
+}
+
+# This password exires every 2 years
+# You'll need to update this in pfSense manually:
+# Services > Acme Certificates > (edit the cert) > Domain SAN list > (expand(+) DNS-Azure (Microsoft) > Client Secret
+resource "azuread_service_principal_password" "pfsense" {
+  service_principal_id = azuread_service_principal.pfsense.id
+  display_name         = "pfsensePassword"
+}
+
+# Lookup existing role asisgnments
+# `az role assignment list --all | grep "<principal_id>" -B10 -A10`
+resource "azurerm_role_assignment" "pfsense_dns_contributor" {
+  scope                = data.azurerm_dns_zone.acmuic_org.id
+  role_definition_name = "DNS Zone Contributor"
+  principal_id         = azuread_service_principal.pfsense.object_id
+}
+
+output "pfsense_service_principal_id" {
+  value = azuread_service_principal.pfsense.id
+}
+
+output "pfsense_service_principal_password" {
+  value     = azuread_service_principal_password.pfsense.value
+  sensitive = true
+}
