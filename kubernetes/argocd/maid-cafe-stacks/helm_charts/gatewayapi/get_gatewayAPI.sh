ff087d539522bffdcdbfce8487cf002af38808de
Lam Nguyen <lamtnguyen989@gmail.com>
2026-07-09 13:38:39 -0500

    changing gatewayAPI install to experimental channel

--- kubernetes/argocd/maid-cafe-stacks/helm_charts/gatewayapi/get_gatewayAPI.sh
+++ kubernetes/argocd/maid-cafe-stacks/helm_charts/gatewayapi/get_gatewayAPI.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-VERSION=1.5.0
-curl -fsSL https://github.com/kubernetes-sigs/gateway-api/releases/download/v${VERSION}/standard-install.yaml -o gateway_api.yml
+VERSION=1.6.0
+CHANNEL=${CHANNEL:-experimental} # Default to experimental channel for TLSRoute support in which Cillium needs
+
+curl -fsSL https://github.com/kubernetes-sigs/gateway-api/releases/download/v${VERSION}/${CHANNEL}-install.yaml \
+        -o gateway_api_${CHANNEL}.yml
